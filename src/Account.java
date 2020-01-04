c9127fdd6c2dee4384275a0e202ce3231f3453b1
manoj <manoj.sarwate@ssiedu.in>
2020-01-04 18:01:58 +0530

    04-01-2020

--- src/Account.java
+++ src/Account.java
@@ -4,13 +4,35 @@ public class Account {
     private int balance;
     private static int totalCash;
     
-    public void showTotalCash(){
+    
+    public  static void showAvg(Account tmp1, Account tmp2, Account tmp3, Account tmp4){
+        //we want to compute avg balance of 3 accounts
+        System.out.println("Avg of 4 : "+(tmp1.balance+tmp2.balance+tmp3.balance+tmp4.balance)/4);
+    }
+    public  static void showAvg(Account tmp1, Account tmp2, Account tmp3){
+        //we want to compute avg balance of 3 accounts
+        System.out.println("Avg of 3 : "+(tmp1.balance+tmp2.balance+tmp3.balance)/3);
+    }
+    public static void showAvg(Account tmp1, Account tmp2){
+        //we want to compute avg balance of two accounts.
+        System.out.println("AVg of 2 : "+(tmp1.balance+tmp2.balance)/2);
+    }
+    
+    public static void drawLine(int n){
+        for(int i=1;i<=n;i++){
+            System.out.print("_");
+        }
+    }
+    public static void showTotalCash(){
         System.out.println("TOTAL CASH : " +totalCash);
     }
+    
     public void setData(int a, String b, int c){
+        //initializing data members of an account
         ano=a;
         name=b;
         balance=c;
+        //managing the total cash of bank.
         totalCash=totalCash+c;
     }
     public void showData(){
--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -6,18 +6,31 @@ public class BankingApplication {
         Account ac1=new Account();
         Account ac2=new Account();
         Account ac3=new Account();
+        Account ac4=new Account();
         
         ac1.setData(111, "AAA", 10000);
         ac2.setData(112, "BBB", 20000);
         ac3.setData(113, "CCC", 30000);
+        ac4.setData(114, "DDD", 40000);
+        
+        /*
+        Account.showData(ac1);
+        Account.showData(ac2);
+        Account.showData(ac3);
+        */
         
         ac1.showData();
         ac2.showData();
         ac3.showData();
+        ac4.showData();
         
-        ac1.showTotalCash();
-        ac2.showTotalCash();
-        ac3.showTotalCash();
+        Account.showAvg(ac1, ac2);
+        Account.showAvg(ac1, ac2, ac3);
+        Account.showAvg(ac1, ac2, ac3, ac4);
         
+        //ac1.showAvg(ac3);
+        
+        //Account.showTotalCash();
+      
     }
 }
